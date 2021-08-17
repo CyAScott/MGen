039ed60b2fe6cfbf4586a5a6bba78a2c38d7a52f
Cy Allen Scott <cy.a.scott@live.com>
2021-08-17 19:28:28 -0400

    feat: added clone tests

---
+++ src/MGen.Tests/Tests/CloningSupport/NestedInterfaceSupport.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace MGen.Tests.CloningSupport
+{
+    public interface INestedCloneChild : ICloneable
+    {
+        Guid Id { get; set; }
+    }
+
+    [Generate]
+    public interface INestedCloneTest : ICloneable
+    {
+        Guid Id { get; set; }
+        INestedCloneChild Child { get; set; }
+    }
+
+    public class NestedInterfaceSupport
+    {
+        [Test]
+        public void Test()
+        {
+            var type = AssemblyScanner.FindImplementationFor<INestedCloneTest>();
+            Assert.IsNotNull(type);
+
+            var instance = (INestedCloneTest)Activator.CreateInstance(type);
+
+            var id = instance.Id = Guid.NewGuid();
+
+            var childType = AssemblyScanner.FindImplementationFor<INestedCloneChild>();
+            Assert.IsNotNull(childType);
+            var child = instance.Child = (INestedCloneChild)Activator.CreateInstance(childType);
+
+            var childId = child.Id = Guid.NewGuid();
+
+            var clone = (INestedCloneTest)instance.Clone();
+
+            Assert.IsNotNull(clone);
+            Assert.IsFalse(ReferenceEquals(instance.Child, clone.Child));
+
+            Assert.AreEqual(id, clone.Id);
+            Assert.AreEqual(childId, clone.Child?.Id);
+        }
+    }
+}
