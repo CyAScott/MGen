c07e4e47df9e8a43806a0bbfd7a7a779effdcbed
Cy Allen Scott <cy.a.scott@live.com>
2021-08-17 19:28:01 -0400

    feat: added assembly scanner helper for tests

---
+++ src/MGen.Tests/AssemblyScanner.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace MGen
+{
+    public static class AssemblyScanner
+    {
+        public static Type FindType(Func<Type, bool> filter) =>
+            typeof(AssemblyScanner)
+                .Assembly
+                .ExportedTypes
+                .SingleOrDefault(filter);
+
+        public static Type FindImplementationFor(Type interfaceType) =>
+            FindType(type =>
+            {
+                if (!type.IsClass ||
+                    type.IsAbstract ||
+                    type.Namespace != interfaceType.Namespace)
+                {
+                    return false;
+                }
+
+                if (!interfaceType.IsGenericTypeDefinition)
+                {
+                    return !type.IsGenericTypeDefinition && interfaceType.IsAssignableFrom(type);
+                }
+
+                if (!type.IsGenericTypeDefinition)
+                {
+                    return false;
+                }
+
+                var interfaceGenericArgs = interfaceType.GetGenericArguments();
+                var typeArgs = type.GetGenericArguments();
+
+                return interfaceGenericArgs.Length == typeArgs.Length;
+            });
+
+        public static Type FindImplementationFor<TInterface>() => FindImplementationFor(typeof(TInterface));
+    }
+}
