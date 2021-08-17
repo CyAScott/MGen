ecb3e307f132ac35fcd6999e79f635b0a7a2e2e4
Cy Allen Scott <cy.a.scott@live.com>
2021-08-17 19:36:19 -0400

    feat: added data binding support tests

---
+++ src/MGen.Tests/Tests/DataBindingSupport/NotifyPropertyChangedSupport.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+using System.ComponentModel;
+
+namespace MGen.Tests.DataBindingSupport
+{
+    [Generate]
+    public interface ISupportNotifyPropertyChanged : INotifyPropertyChanged
+    {
+        Guid Id { get; set; }
+    }
+
+    public class NotifyPropertyChangedSupport
+    {
+        [Test]
+        public void Test()
+        {
+            var type = AssemblyScanner.FindImplementationFor<ISupportNotifyPropertyChanged>();
+            Assert.IsNotNull(type);
+
+            var instance = (ISupportNotifyPropertyChanged)Activator.CreateInstance(type);
+
+            var id = Guid.NewGuid();
+
+            var eventInvoked = false;
+            instance.PropertyChanged += (sender, e) =>
+            {
+                eventInvoked = true;
+                Assert.AreEqual(id, instance.Id);
+            };
+
+            instance.Id = id;
+
+            Assert.IsTrue(eventInvoked);
+        }
+    }
+}
