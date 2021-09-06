57898dd855365981b139911bebe6c97e9eb1e347
Cy Allen Scott <cy.a.scott@live.com>
2021-09-06 14:54:53 -0400

    refactor: renamed parameter

--- src/MGen/Collections/EnumerateBody.cs
+++ src/MGen/Collections/EnumerateBody.cs
@@ -4,10 +4,10 @@
     /// A delegate for building a body within a loop for enumerating through a data structure.
     /// </summary>
     /// <param name="context">The context for building the class.</param>
-    /// <param name="elementValue">The reference to the element value. This reference is readonly and cannot be assigned a different value.</param>
+    /// <param name="getElementValueExpression">The expression statement for reading the element value.</param>
     /// <param name="indices">
     /// The indices for accessing the element.
     /// The indices can be used by the <see cref="CollectionGenerator.Get"/> and <see cref="CollectionGenerator.Set"/> methods to read and write the element value.
     /// </param>
-    public delegate void EnumerateBody(CollectionGenerator context, string elementValue, params string[] indices);
+    public delegate void EnumerateBody(CollectionGenerator context, string getElementValueExpression, params string[] indices);
 }
