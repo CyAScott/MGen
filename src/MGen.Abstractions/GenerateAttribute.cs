5c31557c8cd850a76cc7aa3b730c58fdbdf8e74e
Cy Allen Scott <cy.a.scott@live.com>
2022-03-12 16:27:12 -0500

    refactor: generate attribute

--- src/MGen.Abstractions/GenerateAttribute.cs
+++ src/MGen.Abstractions/GenerateAttribute.cs
@@ -1,26 +1,25 @@
 ﻿using System;
 
-namespace MGen
+namespace MGen;
+
+/// <summary>
+/// Generates a class based on an interface.
+/// </summary>
+[AttributeUsage(AttributeTargets.Interface)]
+public sealed class GenerateAttribute : Attribute
 {
     /// <summary>
-    /// Generates a class based on an interface.
+    /// If true then the interface types for the properties in this interface will also get classes generated.
     /// </summary>
-    [AttributeUsage(AttributeTargets.Interface)]
-    public sealed class GenerateAttribute : MGenAttribute
-    {
-        /// <summary>
-        /// If true then the interface types for the properties in this interface will also get classes generated.
-        /// </summary>
-        public bool GenerateNestedClasses { get; set; } = true;
+    public bool GenerateNestedClasses { get; set; } = true;
 
-        /// <summary>
-        /// The pattern for creating the name for the class.
-        /// </summary>
-        public string DestinationNamePattern { get; set; } = "{{interfaceName | name}}Model";
+    /// <summary>
+    /// The pattern for creating the name for the class.
+    /// </summary>
+    public string DestinationNamePattern { get; set; } = "{{interfaceName | name}}Model";
 
-        /// <summary>
-        /// The <see cref="System.Text.RegularExpressions.Regex"/> pattern for getting values from the interface name.
-        /// </summary>
-        public string SourceNamePattern { get; set; } = @"^(I(?<interfaceName>[A-Z]\w+)|(?<name>\w+))$";
-    }
+    /// <summary>
+    /// The <see cref="System.Text.RegularExpressions.Regex"/> pattern for getting values from the interface name.
+    /// </summary>
+    public string SourceNamePattern { get; set; } = @"^(I(?<interfaceName>[A-Z]\w+)|(?<name>\w+))$";
 }
