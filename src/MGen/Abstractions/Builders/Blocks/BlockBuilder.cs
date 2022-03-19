058413bf34b46908989229d18ede348f6409a293
Cy Allen Scott <cy.a.scott@live.com>
2022-03-19 14:59:28 -0400

    feat: added block builder

---
+++ src/MGen/Abstractions/Builders/Blocks/BlockBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace MGen.Abstractions.Builders.Blocks;
+
+public static partial class CodeBlockExtensions
+{
+    [DebuggerStepThrough]
+    public static BlockBuilder AddBlock(this BlockOfCodeBase parent) => parent.Add(new BlockBuilder(parent));
+}
+
+[DebuggerStepThrough]
+public class BlockBuilder : BlockOfCode
+{
+    internal BlockBuilder(BlockOfCodeBase parent)
+        : base(parent)
+    {
+    }
+
+    protected override void AppendHeader(StringBuilder stringBuilder)
+    {
+    }
+}
